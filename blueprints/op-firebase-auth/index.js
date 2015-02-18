d023d8c52e9e88d224ac640b64b4b24002b85564
Andres Serrano <andres.srg@gmail.com>
2015-02-18 15:58:07 -0500

    Add emberfire to default blueprint

--- blueprints/op-firebase-auth/index.js
+++ blueprints/op-firebase-auth/index.js
@@ -3,6 +3,7 @@ module.exports = {
   },
 
   afterInstall: function() {
+    this.addBowerPackageToProject('emberfire', '~0.0.0');
     return this.addBowerPackageToProject('ember-simple-auth', '0.7.3');
   }
 };
